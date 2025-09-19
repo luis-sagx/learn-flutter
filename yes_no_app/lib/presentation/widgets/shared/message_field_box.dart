023cbe9dea6eaf4869f7d1325545489005845400
Luis Sagnay <sagnayluis99@gmail.com>
2025-09-19 17:51:59 -0500

    text form field

--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,26 +5,37 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final inputDecoration = InputDecoration(
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send),
         onPressed: () {
-          print('Send button pressed');
+          final message = textController.value.text;
+          print('Send button pressed: $message');
+          textController.clear();
         },
       ),
-      hintText: 'Enter your message',
+      hintText: 'End your message with a "?"',
       border: const OutlineInputBorder(
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Message sent: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
-      onChanged: (value) => print('Message typed: $value'),
     );
   }
 }
