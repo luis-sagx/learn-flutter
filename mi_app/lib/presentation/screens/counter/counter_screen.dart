8a20a6bdebb8e7f8ccc8d245348c94134bf23143
Luis Sagnay <sagnayluis99@gmail.com>
2025-09-18 23:30:17 -0500

    counter screen

--- mi_app/lib/presentation/screens/counter/counter_screen.dart
+++ mi_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatelessWidget {
+  final int clickCounter = 0;
+
   const CounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     // Scaffold proporciona una estructura básica para la pantalla, incluyendo AppBar, Body, etc.
-    return const Scaffold(
-      body: Center(
-        child: Text('Counter Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('10',
+                style: TextStyle(fontSize: 150, fontWeight: FontWeight.w100)),
+            Text('Clicks', style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
