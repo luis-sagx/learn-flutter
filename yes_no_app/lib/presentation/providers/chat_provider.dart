4397d1c47f221f5d308880c671c0a482882cd8de
Luis Sagnay <sagnayluis99@gmail.com>
2025-09-21 00:15:05 -0500

    add provider

---
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messageList = [
+    Message(text: 'texto mio', fromWho: FromWho.me),
+    Message(text: 'texto mio 2', fromWho: FromWho.me),
+  ];
+
+  Future sendMessage(String text) async {}
+}
